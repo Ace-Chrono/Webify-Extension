77d954b0d4038e0fe6ebfeb570b02f89df0cc109
Ace-Chrono <richardszhao@gmail.com>
2025-08-25 18:31:00 -0400

    removed logs

--- src/page/siteMods/presets.js
+++ src/page/siteMods/presets.js
@@ -23,7 +23,6 @@ export function getPresets() {
         const currentOrigin = globalState.getCurrentOrigin();
         const currentPreset = presetMap[currentOrigin];
         presetState.setPresetData(currentPreset);
-        console.log(presetState.getPresetData().CSSChanges);
 
         if (presetState.getPresetData()) {
             applyPreset(presetState.getPresetData());
